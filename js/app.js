5e30e944a17bfc4b386f7ce23ad41f1beeb5c78b
ICDL admin <icdl.webmaster.0001@gmail.com>
2021-08-06 12:24:48 +0900

    new rease

---
+++ js/app.js
@@ -0,0 +1,36 @@
+	jQuery(function($) {
+  	$('.slider').slick({
+     arrows: false,
+     variableWidth : true,
+      autoplay: true,
+      infinite: true,
+      autoplaySpeed: 0,
+      cssEase: 'linear',
+      speed: 5000,
+      slidesToScroll: 1,
+      draggable: false,
+      focusOnSelect: false,
+      pauseOnFocus: false,
+  });
+  $slide = $('.slide');
+  $navigation = $('.slide-navigation .item');
+
+  $slide.slick({  //slickスライダー作成
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+  });
+  $navigation.each(function(index){ //サムネイルに連番付与属性
+    $(this).attr('data-number', index);
+  });
+  $navigation.eq(0).addClass('current');  //1枚をオーバーレイ
+
+  $navigation.on('click', function(){ //サムネイルクリック時イベント
+    var number = $(this).attr('data-number');
+    $slide.slick('slickGoTo', number, true);
+    $(this).siblings().removeClass('current');
+    $(this).addClass('current');
+  });
+});
